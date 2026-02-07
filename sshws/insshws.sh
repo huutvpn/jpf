24e0414673d95e3eecab7a258578723ed94d2596
huutvpn <143250398+huutvpn@users.noreply.github.com>
2026-02-07 19:23:57 +0700

    Add files via upload

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -7,7 +7,7 @@ cd
 #wget -O /usr/local/bin/ws-openssh bebejje
 wget -O /usr/local/bin/ws-dropbear -##
 wget -O /usr/local/bin/ws-stunnel 8-2728+383+
-#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/huutvpn/instalasi/sshws/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
+#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/huutvpn/jp/sshws/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
 #chmod +x /usr/local/bin/ws-openssh
