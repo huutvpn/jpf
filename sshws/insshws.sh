dfeda9adb34cbb07d07931d45c08ebe5e35dedb2
huutvpn <143250398+huutvpn@users.noreply.github.com>
2026-02-07 19:11:33 +0700

    Add files via upload

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -4,13 +4,13 @@
 cd
 
 #Install Script Websocket-SSH Python
-#wget -O /usr/local/bin/ws-jpssh bebejje
+#wget -O /usr/local/bin/ws-openssh bebejje
 wget -O /usr/local/bin/ws-dropbear -##
 wget -O /usr/local/bin/ws-stunnel 8-2728+383+
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/huutvpn/jp/sshws/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
-#chmod +x /usr/local/bin/ws-jpssh
+#chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel 
 egebenenebsy
